8dc216554961f5e2232dafd5653daaa13a07f873
sszhu <nora.shasha.zhu@gmail.com>
2016-02-02 17:09:05 +0800

    u

---
+++ src/js/private/auction.js
@@ -0,0 +1,31 @@
+$(function(){
+  var config = {
+    expression = [
+      '这瓶82年的拉菲我势在必得，祝福我吧。',
+      'not':'我中奖了,发几个红包大家一起来玩'
+    ]
+  }
+  var args = app.methods.getSearchArgFromUrl()
+  function userData(){
+    // var res = {}
+    // for (var pro in args) {
+    //   res.pro = args[pro]
+    // }
+    var res = {
+      'userId':'800EA222D462D4F8809AE24214E27329',
+      'userType':'1',
+      'bid':1
+    }
+    return res
+  }
+  app.methods.appAjax('请求夺宝详情接口',
+                      'bbinfo',
+                      'POST',
+                      userData(),
+                      app.methods.timestamp(),
+                      function(response){
+                        if (response.type == 1) {
+                          
+                        }
+                      })
+})
