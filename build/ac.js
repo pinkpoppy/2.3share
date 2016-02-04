8de1bbfdc942c7544d8df8fe3a8a19400449d112
sszhu <nora.shasha.zhu@gmail.com>
2016-02-04 16:28:07 +0800

    u

--- build/ac.js
+++ build/ac.js
@@ -37,4 +37,157 @@ $(function(){
                                 })
 
     })
+
+    
+//bootstrap
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+sina CDN
+http://lib.sinaapp.com/?path=/jquery
+
+
+服务器目录:
+服务器日志:/usr/local/httpd/logs
+域名对应的目录:
+www.pai9.com.cn => /home/www/default/
+www.snapwine.net => /app/
+
+SVN系统:
+zhushasha 0HU9dckm<
+
+测试版后台管理系统
+http://124.205.208.198/pjapi/ms/login/
+zss abc123
+正式版后台管理系统
+www.snapwine.net/pjapi/ms/
+登录名: zh1234 密码:1234
+clzhang a
+
+http://p.getqujing.com/paths/7zn23id1.pac
+
+http://wenku.baidu.com/view/e8c07edb6f1aff00bed51ed7
+
+//正式服务上的测试数据库
+mysql -uroot -pfQHvxT6ytETyAa9R 
+
+use winesite_test
+/usr/local/httpd/logs
+service httpd restart
+
+hhwordpress:
+notonlysuccess
+bcsW!680
+
+正式服务:
+ssh -p 3322 root@121.42.40.196
+jvevW6wD5’
+
+测试服务:
+124.205.208.198:33222
+ssh -p 33222 root@124.205.208.198
+yunshitu!
+mysql -uroot -p73YX0DtF
+数据库就用winesite
+
+密码：73YX0DtF
+目录:/data/www/weixin.yunshitu.cn/activity
+
+
+zhuchaopaijiu 67275891
+
+微信公众平台:
+服务号：
+账号pai9@pai9.com.cn
+密码mppaipai101
+开发者ID
+AppID(应用ID)wxeb681a427246a6c2
+AppSecret(应用密钥)df8799df884e21bb80e284975e7d4b5e 隐藏 重置
+
+
+
+
+/data/www/weixin.yunshitu.cn/activity
+黄祖林  13:03:27
+站点名字为weixin.yunshitu.cn
+
+论酒后台:
+dadamao39
+121636
+
+********** 新的测试服务器 开始 **********
+地址:124.205.208.198 33322
+用户名:root 
+密码:yunshitu!
+数据库: winesite
+数据库端口:3307
+数据库密码:1qaz2wsx
+********* 新的测试服务器 结束 **********
+
+scp -P33322 Downloads/site.zip root@124.205.208.198:/data/www/
+scp -P33322 local_file root@124.205.208.198:/data/www/
+php网站路径：/data/www
+网址：http://www.yaerku.com/
+张春森  19:10:39
+http://www.yaerku.com/phpinfo.php
+新的测试服务器 结束
+
+********** 去哪网登陆密码 开始**********
+hupc8365
+********** 去哪网登陆密码 结束**********
+http://www.regexr.com/
+
+
+正式服务
+北京IDC: 公网 IP:116.90.85.77 端口:33322 system centos 6.7*64 
+root
+mjJEv97*km!
+
+
+paijiuwx@163.com
+
+pai9@pai9.com.cn mppaipai101
+
+dadamao39
+121636
+
+
+微猫后台运营:
+15330275808
+密码wmart67275891
+
+ping++ 支付
+snapwine@163.com
+pkjxpay
+
+
+http://t.snapwine.net:7784
+ssh root@192.168.1.7
+密码：1qaz2wsx
+路径： /data/www
+
+
+103.224.250
+pptp
+shanghai5        lcm7*p2ME
+黄祖林  13:57:49
+103.224.250.100
+
+
+
+http://t.snapwine.net:7784/pjms/swmp/index.php
+zc
+1234
+
+
+应用宝下载链接
+http://a.app.qq.com/o/simple.jsp?pkgname=com.snapwine.snapwine
+
+服务号-拍酒吧
+账号paijiuwx@163.com
+密码PaiJiu168  
+APPID wx9d7691ebcd3f48bc
+SECRET b4fe3e0a53db4affaf347d927563a4d7
 })
