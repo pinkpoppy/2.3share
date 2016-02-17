9dd6dc24edb6253588a3f7f179aaca375689260c
sszhu <nora.shasha.zhu@gmail.com>
2016-02-17 18:17:52 +0800

    m

--- build/auction.php
+++ build/auction.php
@@ -69,7 +69,8 @@
 
   <script type="text/javascript">
   function myFadeIn(){
-    createMask()
+    var parent = document.querySelector('body')
+    createMask(parent)
     var mask = document.querySelector('#mask')
     var popTips = document.querySelector('#test')
     if (!mask.style.opacity && !popTips.style.opacity) {
@@ -79,7 +80,7 @@
     var maskInterval = setInterval(function(){
       var preOpacity = Number(mask.style.opacity)
       mask.style.opacity = preOpacity + 0.02
-      if (mask.style.opacity >=0.5) {
+      if (mask.style.opacity >=0.6) {
         clearInterval(maskInterval)
       }
     },50)
@@ -90,19 +91,19 @@
       if (popTips.style.opacity >=1) {
         clearInterval(popTipsInterval)
       }
-    },50)
+    },25)
   }
   function createMask(parent){
     var rules = "两个穿着西服、衬衫的男子，每人手里拿着一把20厘米左右的匕首，其中一个十八九岁的男子，身高约1.7米，肩膀很厚实，身上沾了不少血。后来，冷婧才知道，这名男子在打斗过程中，被公公咬断了右手中指。另一个男子，坐在进门的沙发椅上。冷婧一眼认出了他——一家连锁房产中介的业务员钟杰。这个颧骨很高、很瘦的男子，30多岁，眼睛有些凹陷，给人一种很凶相的感觉。冷婧家的房子曾通过他挂牌出售，他也曾带冷婧夫妇看过三四套房子。"
-    var maskHeight = Number(parent.style.clientHeight)
+    var maskHeight = Number(parent.offsetHeight)
     var mask = document.createElement('div')
 
     var maskId = document.createAttribute('id')
-    maskId.value = mask
+    maskId.value = 'mask'
     mask.setAttributeNode(maskId)
-    var maskStyle = document.createAttribute('style')
-    maskStyle.value = 'style',"'opacity:0;height:"+ maskHeight + ";'"
-    mask.setAttributeNode(maskStyle)
+    mask.style.opacity = 0
+    console.log(maskHeight)
+    mask.offsetHeight = maskHeight
     parent.appendChild(mask)
 
 
@@ -111,10 +112,7 @@
     var popId = document.createAttribute('id')
     popId.value = 'test'
     popTips.setAttributeNode(popId)
-
-    var popStyle = document.createAttribute('style')
-    popStyle.value = 'opacity:0;'
-    popTips.setAttributeNode(popStyle)
+    popTips.style.opacity = 0
     popTips.innerHtml = rules
     mask.appendChild(popTips)
   }
